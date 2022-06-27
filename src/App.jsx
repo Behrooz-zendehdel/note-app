bb3f6c4dc13ff529922c487e54de1b4b2c69c156
behrooz <behrooz.zendedel1992@gmail.com>
2022-06-27 11:25:11 +0430

    build strcture noteList & note

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NoteList from "./Components/NoteList";
+
+const App = () => {
+  return (
+    <div>
+      <div className="container">
+        <NoteList />
+      </div>
+    </div>
+  );
+};
+
+export default App;
